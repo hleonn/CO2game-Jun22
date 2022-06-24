7435ed126aa2b29b1272951af17c22d008028666
hleonn <edhleon2001@hotmail.com>
2022-06-23 20:47:12 -0500

    fix gh pages paths

--- javascript/clasePoka.js
+++ javascript/clasePoka.js
@@ -1,7 +1,7 @@
 class Poka extends Actors {
     constructor(x, y, ctx) {
         let image = new Image()
-        image.src = "../img/mr_poka.png";//"../img/mr_poka.png"
+        image.src = "./img/mr_poka.png";//"./img/mr_poka.png"
 
         super(x, y, 80, ctx, image)
     }
@@ -10,7 +10,7 @@ class Poka extends Actors {
 class GameTrack {
     constructor(width, height, ctx) {
         let image = new Image()
-        image.src = "../img/track.jpeg";
+        image.src = "./img/track.jpeg";
         ctx.drawImage(image, 0, 0, width, height);
     }
 }
@@ -18,7 +18,7 @@ class GameTrack {
 class Hamburger extends Actors {
     constructor(x, y, ctx) {
         let image = new Image()
-        image.src = "../img/hamburger2.png";
+        image.src = "./img/hamburger2.png";
         super(x, y,50, ctx, image)
     }
 }
@@ -26,35 +26,35 @@ class Hamburger extends Actors {
 class Coca extends Actors {
     constructor(x, y, ctx) {
         let image = new Image()
-        image.src = "../img/coca1.png";
+        image.src = "./img/coca1.png";
         super(x, y,50, ctx, image)//50 Coca Size
     }
 }
 class Donuts extends Actors {
     constructor(x, y, ctx) {
         let image = new Image()
-        image.src = "../img/donuts1.png";
+        image.src = "./img/donuts1.png";
         super(x, y,50, ctx, image)
     }
 }
 class Potatos extends Actors {
     constructor(x, y, ctx) { 
         let image = new Image()
-        image.src = "../img/papitas1.png";
+        image.src = "./img/papitas1.png";
         super(x, y,80, ctx, image)
     }
 }
 class Joku extends Actors {//Mr Joku=Mr Joke
     constructor(x, y, ctx) {
         let image = new Image()
-        image.src = "../img/joku.png";
+        image.src = "./img/joku.png";
         super(x, y,200, ctx, image)
     }
 }
 class Rat extends Actors {
     constructor(x, y, ctx) {
         let image = new Image()
-        image.src = "../img/rat.png";
+        image.src = "./img/rat.png";
         super(x, y, Rat.size, ctx, image)
     }
 
